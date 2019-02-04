326b8e06599e01562e3f0c950796677e0d06e9fe
Chance Payne <chance@lambdaschool.com>
2019-02-04 10:02:28 -0700

    Initial commit

---
+++ app/src/main/java/com/lambdaschool/guidedproject_customview_movecharacter/MainActivity.java
@@ -0,0 +1,13 @@
+package com.lambdaschool.guidedproject_customview_movecharacter;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
